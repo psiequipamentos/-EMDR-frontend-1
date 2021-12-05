a7b676a357397a46c8e9b13433f3d175d29eeb30
Dorishow <jsantosalmeida54@gmail.com>
2021-12-05 14:59:39 -0300

    Iniciando a perfumaria

--- src/components/modals/DragDropModal.tsx
+++ src/components/modals/DragDropModal.tsx
@@ -89,8 +89,8 @@ dragElement(elmnt: any) {
                         <div id="mydiv" className="fixed top-0 left-0" ref={this.state.draggableDiv} >
                             <div className="relative w-full mx-auto my-6">
                                 <div className="relative flex flex-col w-full border-0 rounded-lg outline-none focus:outline-none">
-                                    <article className="relative mx-auto overflow-auto bg-gray-100 rounded lg:w-1/2">
-                                    <div id="mydivheader">Clique aqui para mover o chat</div>
+                                    <article className="relative w-full mx-auto overflow-auto bg-gray-100 rounded">
+                                    <div className="" id="mydivheader">Clique aqui para mover o chat</div>
                                         {this.props.content ?
                                             <this.props.content closeModal={this.changeTrigger} socket={this.props.socket} trigger={this.changeTrigger}/>
                                             : this.props.children
---
+++ src/pages/emdr/icons.tsx
@@ -0,0 +1,22 @@
+export const muteMicrofone = <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mx-auto text-red-700" viewBox="0 0 20 20" fill="currentColor">
+<path fill-rule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clip-rule="evenodd" />
+</svg>
+
+export const hide = <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+</svg>
+
+export const show = <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+</svg>
+
+export const play = <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+
+export const pause = <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+
