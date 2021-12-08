6f955fe43f0431f860e7ca1cbd113559ebb2bf59
Dorishow <jsantosalmeida54@gmail.com>
2021-12-08 10:02:29 -0300

    Estilização da tela de PreJoin

--- src/pages/emdr/prejoin.tsx
+++ src/pages/emdr/prejoin.tsx
@@ -1,4 +1,5 @@
 import DailyIframe, { DailyParticipantsObject } from "@daily-co/daily-js";
+import "../../styles/prejoin.css"
 import axios from "axios";
 import React from "react";
 
@@ -142,14 +143,15 @@ export default class Prejoin extends React.Component<any, iStates> {
   render() {
     return (
       <React.Fragment>
-        <video className="video-large" autoPlay={true}></video>
-        <h2>URL: {this.state.url}</h2>
+        <video className="prejoin-camera" autoPlay={true}></video>
+        {/* <h2>URL: {this.state.url}</h2>
         <button onClick={this.createCall}>criar link</button>
-        <br />
-        <button onClick={() => this.joinCall(this.state.url)}>Entrar</button>
-        <input onChange={this.handleChange} name="url"></input>
+        <br /> */}
+        <button className="px-5 py-3 m-5 text-sm font-semibold text-white bg-gray-900 border border-gray-300 rounded hover:shadow-lg hover:bg-gray-800 hover:text-gray-100" onClick={() => this.joinCall(this.state.url)}>Entrar</button>
+        {/* <input onChange={this.handleChange} name="url"></input> */}
         {/*   <button onClick={this.entrar}>join</button>
         <button onClick={this.participants}>participantes </button> */}
+        <p className="text-white">https://twitter.com/Rukaszu/status/1468348807322681345</p>
         <div id="users-container"></div>
       </React.Fragment>
     );
