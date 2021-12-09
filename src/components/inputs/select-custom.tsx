8512326988e3e9ce078afc8e4e7829477e101989
Dorishow <jsantosalmeida54@gmail.com>
2021-12-09 10:30:40 -0300

    Modal de edição de paciente e bugfix do select sem name

--- src/components/inputs/select-custom.tsx
+++ src/components/inputs/select-custom.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 interface ISelectProps{
   options: any
   handleChange: any
+  name: any
 }
 
 export default class SelectCustom extends React.Component<ISelectProps, any>{
@@ -12,7 +13,7 @@ export default class SelectCustom extends React.Component<ISelectProps, any>{
 
   render(){
     return(
-      <select className="p-2 text-sm font-semibold text-black bg-gray-100 rounded outline-none" name="sound" onChange={this.props.handleChange}>
+      <select className="p-2 text-sm font-semibold text-black bg-gray-100 rounded outline-none" name={this.props.name} onChange={this.props.handleChange}>
           {this.props.options.map(
             (item: any, index: any) => <option className="p-3 text-gray-900" key={index + 'sound'} value={item.value}>{item.name}</option>
           )}
--- src/pages/home/navbar/navbar.tsx
+++ src/pages/home/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default class NavbarMenu extends React.Component{
   render(){
     return(
-      <nav className="flex justify-between p-3 text-sm font-semibold shadow">
+      <nav className="flex flex-wrap justify-between p-3 text-sm font-semibold shadow">
         <img className="p-3 px-10 bg-gray-200" src="" alt="Logo" />
         <div className="flex justify-end">
           <h2 className="self-center mx-5">Nome Sobrenome</h2>
