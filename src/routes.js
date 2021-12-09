fda71e6bddd9db697bd59aef9de7a13262dd1d31
Dorishow <jsantosalmeida54@gmail.com>
2021-12-08 22:14:45 -0300

    Formulário de cadastro de psicólogo

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import Cadastro from './pages/cadastroPsicologo/cadastro';
 import Emdr from './pages/emdr/emdr';
 import Prejoin from './pages/emdr/prejoin';
 import HomePage from './pages/home';
@@ -10,6 +11,7 @@ function RouterComponent() {
     <div className="App">
         <Routes>
           <Route path='home' element={<HomePage />}> </Route>
+          <Route path='cadastro' element={<Cadastro />}> </Route>
           <Route path='prejoin' element={<Prejoin />}> </Route>
           <Route path="emdr/psicologo" element={<Emdr ControlsVisibility={true} />}></Route>
           <Route path="emdr/paciente" element={<Emdr ControlsVisibility={false} />}></Route>
